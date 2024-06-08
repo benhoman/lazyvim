6ac2e543c74ec522e2bae3d6b40c2b58239172d2
Ben Homan <ben@benhoman.com>
2024-06-08 00:31:21 -0400

    Initial commit

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,15 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local opt = vim.opt
+
+opt.autowrite = false
+opt.clipboard = "unnamed"
+opt.incsearch = true
+opt.scrolloff = 8
+opt.colorcolumn = "80,90,100"
+opt.ruler = true
+
+opt.swapfile = false
+opt.backup = false
+opt.undodir = os.getenv("HOME") .. "/.cache/nvim/undodir"
---
+++ lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,4 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = { format = { timeout_ms = 3000 } },
+}
