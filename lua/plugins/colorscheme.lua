d66094a1a53a57e8768ee1daa7b2fe7e3ca21f17
Ben Homan <ben@benhoman.com>
2024-06-10 15:10:15 -0400

    add catppuccin

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "moon" },
+    enabled = false,
+  },
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+    opts = {
+      flavour = "mocha",
+      -- flavour = "latte",
+      -- flavour = "auto", -- latte, frappe, macchiato, mocha
+      -- background = { -- :h background
+      --   light = "latte",
+      --   dark = "mocha",
+      -- },
+    },
+  },
+}
