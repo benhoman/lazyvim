3643b971d9bebef0c8e16e3934ac52a88c3b123d
Ben Homan <ben@benhoman.com>
2025-03-27 22:38:39 -0400

    updates

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,22 +1,25 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    lazy = true,
-    opts = { style = "moon" },
-    enabled = true,
-  },
   -- {
-  --   "catppuccin/nvim",
+  --   "folke/tokyonight.nvim",
   --   lazy = true,
-  --   name = "catppuccin",
   --   opts = {
-  --     -- flavour = "mocha",
-  --     -- flavour = "latte",
-  --     flavour = "auto", -- latte, frappe, macchiato, mocha
-  --     background = { -- :h background
-  --       light = "latte",
-  --       dark = "mocha",
-  --     },
+  --     style = "moon",
   --   },
+  --   enabled = true,
   -- },
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+    enabled = true,
+    opts = {
+      flavour = "mocha",
+      -- flavour = "latte",
+      -- flavour = "auto", -- latte, frappe, macchiato, mocha
+      background = { -- :h background
+        light = "latte",
+        dark = "mocha",
+      },
+    },
+  },
 }
