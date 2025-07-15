874b643701c23a5ef8ea79f67c8506fc9bd5367d
Ben Homan <ben@benhoman.com>
2025-07-15 10:05:57 -0400

    add hypr lsp/treesitter

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,4 +1,9 @@
 return {
   "neovim/nvim-lspconfig",
-  opts = { format = { timeout_ms = 3000 } },
+  opts = {
+    format = { timeout_ms = 3000 },
+    servers = {
+      hyprls = {},
+    },
+  },
 }
